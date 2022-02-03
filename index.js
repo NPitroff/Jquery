cc328b3e1410134c48ff4cf9a64f8a1eb6ba3b0f
NPitroff <nicolai.pitroff@gmail.com>
2022-02-03 07:10:22 -0800

    Added side by side to show code with & without jquery

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // Test to make sure Jquery is working
-$("h1").css("color", "red");
+// $("h1").css("color", "red");
+
+//Code to change h1 to purple without Jquery
+// for (var i=0; i<5; i++){
+//   document.querySelectorAll("button")[i].addEventListener("click", function(){
+//     document.querySelector("h1").style.color = "purple";
+//   })
+// }
+
+// Code to change h1 to purple with Jquery
+$("button").click(function(){
+  $("h1").css("color", "purple");
+});
