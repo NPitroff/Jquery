07d05d7f844bffa31d8867d9495105a37272b80d
NPitroff <nicolai.pitroff@gmail.com>
2022-02-03 07:24:50 -0800

    Incorporated the on() method

--- index.js
+++ index.js
@@ -12,3 +12,19 @@
 $("button").click(function(){
   $("h1").css("color", "purple");
 });
+
+
+// detect keystrokes inside the textbox
+$("input").keydown(function(e){
+  console.log(e.key);
+});
+
+// change the h1 to the keystrokes
+$(document).keydown(function(e){
+  $("h1").text(e.key);
+});
+
+// Adding an event listener with the 'on()' method
+$(document).on("mouseover", function(){
+  $("h1").css("color", "green")
+});
